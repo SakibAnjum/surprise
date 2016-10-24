7a28642d5bdcc8ad3a8080c23af1b32215210b09
SakibAnjum <sakib.ask@gmail.com>
2016-10-24 18:56:54 +0600

    file added

--- js/main.js
+++ js/main.js
@@ -1,24 +1,37 @@
 (function ($) {
  "use strict";
 
-/*----------------------------
- jQuery MeanMenu
------------------------------- */
-	jQuery('nav#dropdown').meanmenu();	
-	
+
+
+
+
+
+ /*---------------------
+	 countdown  activation
+	--------------------- */
+	$('[data-countdown]').each(function() {
+	  var $this = $(this), finalDate = $(this).data('countdown');
+	  $this.countdown(finalDate, function(event) {
+		$this.html(event.strftime('<span class="cdown days"><span class="time-count">%-D</span> <p>Days</p></span> <span class="cdown hour"><span class="time-count">%-H</span> <p>Hour</p></span> <span class="cdown minutes"><span class="time-count">%M</span> <p>Min</p></span> <span class="cdown second"><span class="time-count">%S</span> <p>Sec</p></span>'));
+	  });
+	});
+
+
+
+
 /*----------------------------
  wow js active
 ------------------------------ */
  new WOW().init();
- 
+
 /*----------------------------
  owl active
------------------------------- */  
+------------------------------ */
   $("#owl-demo").owlCarousel({
-      autoPlay: false, 
+      autoPlay: false,
 	  slideSpeed:2000,
 	  pagination:false,
-	  navigation:true,	  
+	  navigation:true,
       items : 4,
 	  /* transitionStyle : "fade", */    /* [This code for animation ] */
 	  navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
@@ -30,27 +43,27 @@
 
 /*----------------------------
  price-slider active
------------------------------- */  
+------------------------------ */
 	  $( "#slider-range" ).slider({
 	   range: true,
 	   min: 40,
 	   max: 600,
 	   values: [ 60, 570 ],
 	   slide: function( event, ui ) {
-		$( "#amount" ).val( "£" + ui.values[ 0 ] + " - £" + ui.values[ 1 ] );
+		$( "#amount" ).val( "ï¿½" + ui.values[ 0 ] + " - ï¿½" + ui.values[ 1 ] );
 	   }
 	  });
-	  $( "#amount" ).val( "£" + $( "#slider-range" ).slider( "values", 0 ) +
-	   " - £" + $( "#slider-range" ).slider( "values", 1 ) );  
-	   
+	  $( "#amount" ).val( "ï¿½" + $( "#slider-range" ).slider( "values", 0 ) +
+	   " - ï¿½" + $( "#slider-range" ).slider( "values", 1 ) );
+
 /*--------------------------
  scrollUp
----------------------------- */	
+---------------------------- */
 	$.scrollUp({
         scrollText: '<i class="fa fa-angle-up"></i>',
         easingType: 'linear',
         scrollSpeed: 900,
         animation: 'fade'
-    }); 	   
- 
-})(jQuery); 
+    });
+
+})(jQuery);
