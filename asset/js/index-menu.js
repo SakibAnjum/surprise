8566c40a75fa99c1985423cd6ace79f5893c8170
SakibAnjum <sakib.ask@gmail.com>
2016-11-02 16:01:46 +0600

    CountDown Solved

--- asset/js/index-menu.js
+++ asset/js/index-menu.js
@@ -10,12 +10,46 @@
           event.preventDefault();
         });
     });
-
-
     $('.menu-button').on('click',function(){
       $('.menu-button').toggleClass('open');
       $('.menu-button').toggleClass('clse');
       $('.menu').toggleClass('open');
       $('.menu').toggleClass('clse');
     });
+  $(".testimonial-carousel").owlCarousel({
+      autoPlay: true,
+	    slideSpeed:2000,
+	    pagination:true,
+	    navigation:false,
+      mouseDrag:false,
+      items : 1,
+	    transitionStyle : "backSlide",
+	    navigationText:["<i class='fa fa-angle-left'></i>","<i class='fa fa-angle-right'></i>"],
+      itemsDesktop : [1199,1],
+	    itemsDesktopSmall : [980,1],
+	    itemsTablet: [768,1],
+	    itemsMobile : [479,1],
+  });
+
+
+//=> Scroll Header Activation
+var mywindow = $(window);
+var mypos = 0;
+var up = false;
+var newscroll;
+mywindow.scroll(function () {
+   newscroll = mywindow.scrollTop();
+   if (newscroll > mypos && !up || newscroll <= 0 ) {
+     $('.scroll-header').addClass('clse');
+       up = !up;
+   }
+
+   else if(newscroll < mypos && up) {
+       $('.scroll-header').removeClass('clse');
+       up = !up;
+   }
+
+   mypos = newscroll;
+});
+
  })(jQuery);
