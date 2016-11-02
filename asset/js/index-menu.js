e11678593dcda5366e58c69889d9f5ed252c1406
SakibAnjum <sakib.ask@gmail.com>
2016-11-02 14:31:07 +0600

    Final Submissin

--- asset/js/index-menu.js
+++ asset/js/index-menu.js
@@ -1,22 +1,21 @@
-/* one page active  */
-   $("#nav").onePageNav();
-
-   //jQuery for page scrolling feature - requires jQuery Easing plugin
- jQuery(function($) {
-   $('#nav ul li a').bind('click', function(event) {
-     var $anchor = $(this);
-     $('html').stop().animate({
-       scrollTop: $($anchor.attr('href')).offset().top - 0
-     }, 1000, 'easeInOutQuint');
-     event.preventDefault();
-   });
- });
-
+(function ($) {
+  "use strict";
+    $("nav.menu").onePageNav();
+      jQuery(function($) {
+        $('#nav ul li a').bind('click', function(event) {
+          var $anchor = $(this);
+          $('html').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top - 0
+          }, 1000, 'easeInOutQuint');
+          event.preventDefault();
+        });
+    });
 
 
- $('.menu-button').on('click',function(){
-   $('.menu-button').toggleClass('open');
-   $('.menu-button').toggleClass('clse');
-   $('.menu').toggleClass('open');
-   $('.menu').toggleClass('clse');
- });
+    $('.menu-button').on('click',function(){
+      $('.menu-button').toggleClass('open');
+      $('.menu-button').toggleClass('clse');
+      $('.menu').toggleClass('open');
+      $('.menu').toggleClass('clse');
+    });
+ })(jQuery);
